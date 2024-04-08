ad1e4df977cc638205fb91796c5372140ac1f0af
Pihu@141 <vaishnavichavale1@gmail.com>
2024-04-08 13:05:57 +0530

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {path:'budget-planner', loadChildren:()=> import('./budget-planner/budget-planner.module').
+    then(m=>m.BudgetPlannerModule)}
+];
---
+++ src/app/budget-planner/budget-planner.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule,Routes } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+const routes:Routes=[
+  {path:'login', component:LoginComponent},
+  {path:'side-nav',component:SideNavComponent},
+  {path:'dashboard',component:DashboardComponent}
+
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+   
+   RouterModule.forChild(routes)
+  ],
+  exports:[RouterModule]
+})
+export class BudgetPlannerModule { }
